3b20790cb9d6b27016f9a3c792a9a2700490a0a3
Marcus Dillavou <dillavou@vipaar.com>
2018-11-01 15:47:25 -0500

    Updated info based on the hpid api

--- lib/ueberauth/strategy/hpid.ex
+++ lib/ueberauth/strategy/hpid.ex
@@ -154,26 +154,10 @@ defmodule Ueberauth.Strategy.HPID do
 
     %Info{
       name: user["name"],
-      description: user["bio"],
-      nickname: user["login"],
-      email: fetch_email!(user),
-      location: user["location"],
-      image: user["avatar_url"],
-      urls: %{
-        followers_url: user["followers_url"],
-        avatar_url: user["avatar_url"],
-        events_url: user["events_url"],
-        starred_url: user["starred_url"],
-        blog: user["blog"],
-        subscriptions_url: user["subscriptions_url"],
-        organizations_url: user["organizations_url"],
-        gists_url: user["gists_url"],
-        following_url: user["following_url"],
-        api_url: user["url"],
-        html_url: user["html_url"],
-        received_events_url: user["received_events_url"],
-        repos_url: user["repos_url"]
-      }
+      first_name: user["given_name"],
+      last_name: user["family_name"],
+      nickname: user["sub"],
+      email: user["email"],
     }
   end
 
@@ -199,36 +183,19 @@ defmodule Ueberauth.Strategy.HPID do
   end
 
   defp fetch_email!(user) do
-    user["email"] || get_primary_email!(user)
-  end
-
-  defp get_primary_email!(user) do
-    unless user["emails"] && Enum.count(user["emails"]) > 0 do
-      raise "Unable to access the user's email address"
-    end
-
-    Enum.find(user["emails"], & &1["primary"])["email"]
+    user["email"]
   end
 
   defp fetch_user(conn, token) do
     conn = put_private(conn, :hpid_token, token)
     # Will be better with Elixir 1.3 with/else
-    case Ueberauth.Strategy.HPID.OAuth.get(token, "/user") do
+    case Ueberauth.Strategy.HPID.OAuth.get(token, "/userinfo") do
       {:ok, %OAuth2.Response{status_code: 401, body: _body}} ->
         set_errors!(conn, [error("token", "unauthorized")])
 
       {:ok, %OAuth2.Response{status_code: status_code, body: user}}
       when status_code in 200..399 ->
-        case Ueberauth.Strategy.HPID.OAuth.get(token, "/user/emails") do
-          {:ok, %OAuth2.Response{status_code: status_code, body: emails}}
-          when status_code in 200..399 ->
-            user = Map.put(user, "emails", emails)
-            put_private(conn, :hpid_user, user)
-
-          # Continue on as before
-          {:error, _} ->
-            put_private(conn, :hpid_user, user)
-        end
+        put_private(conn, :hpid_user, user)
 
       {:error, %OAuth2.Error{reason: reason}} ->
         set_errors!(conn, [error("OAuth2", reason)])
